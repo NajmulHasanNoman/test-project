1449c2b2af974efbeefdff9f3bdd68f216163088
NajmulHasanNoman <101030554+NajmulHasanNoman@users.noreply.github.com>
2025-01-01 12:42:26 +0600

    name chage

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            <h1>This is product page</h1>
+            <h1>This is product age  change</h1>
         </div>
     );
 };
